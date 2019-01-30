97d5bb8c8a72541a751ed2956a09f9baa8295297
LuZhiqiang <luzhiqiang0405@autohome.com.cn>
2019-01-30 17:28:41 +0800

    feat(test): 增加测试

--- sp/src/test/java/luzq/std/jstd/AppTest.java
+++ sp/src/test/java/luzq/std/jstd/AppTest.java
@@ -1,20 +1,38 @@
 package luzq.std.jstd;
 
-import static org.junit.Assert.assertTrue;
-
+import luzq.std.jstd.sp.bean.condition.LinuxBean;
+import luzq.std.jstd.sp.bean.condition.SystemBean;
+import luzq.std.jstd.sp.config.MainConfig;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MainConfig.class})
+@ActiveProfiles("prod")
+public class AppTest {
+    @Autowired
+    SystemBean systemBean;
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testCondition() {
+        systemBean.printName();
+        assertTrue(systemBean instanceof LinuxBean);
     }
 }
